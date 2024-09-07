84507f8b096e2dc4fc4c5134694c26fd1ae68e3a
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-09-07 17:38:23 -0300

    feat: trying a different strategy

--- client/pages/Page/action/index.ts
+++ client/pages/Page/action/index.ts
@@ -2,34 +2,47 @@
 
 import { YoutubeLoader } from "@langchain/community/document_loaders/web/youtube";
 
+import { YoutubeTranscript } from "youtube-transcript";
+
 export interface Action {
   (url: string): Promise<{
     content: string;
-    description: string;
-    title: string;
-    author: string;
+    description?: string;
+    title?: string;
+    author?: string;
   }>;
 }
 
 export const action: Action = async (url: string) => {
-  const loader = YoutubeLoader.createFromUrl(url, {
-    addVideoInfo: true,
-  });
+  const transcription = await YoutubeTranscript.fetchTranscript(url);
 
-  const docs = await loader.load();
+  const textArray = transcription.map((tr) => tr.text);
+  const fullText = textArray.join(" ");
 
-  const video = docs[0];
-  const content = video.pageContent;
-  const description = video.metadata.description;
-  const title = video.metadata.title;
-  const author = video.metadata.author;
+  console.log("[fulltext]", fullText);
 
-  const result = {
-    content,
-    description,
-    title,
-    author,
+  return {
+    content: fullText,
   };
 
-  return result;
+  //   const loader = YoutubeLoader.createFromUrl(url, {
+  //     addVideoInfo: true,
+  //   });
+
+  //   const docs = await loader.load();
+
+  //   const video = docs[0];
+  //   const content = video.pageContent;
+  //   const description = video.metadata.description;
+  //   const title = video.metadata.title;
+  //   const author = video.metadata.author;
+
+  //   const result = {
+  //     content,
+  //     description,
+  //     title,
+  //     author,
+  //   };
+
+  //   return result;
 };
