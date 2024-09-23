ec79170284669d8a91029fd1788b4df34df2e0d1
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-09-23 20:15:34 -0300

    remove: url

--- app/app/shop/api/route.ts
+++ app/app/shop/api/route.ts
@@ -1,5 +1,5 @@
 import { companyServices } from "@/server/services/company";
-import { NextApiRequest } from "next";
+
 import { NextRequest, NextResponse } from "next/server";
 
 import Stripe from "stripe";
